e7408d40e39b0cfba255fa6fd83fd0e96c03876a
Kevin Mitchell <kevin@infielddesign.com>
2014-02-13 17:33:19 -0800

    added some grunt tools for compacting and minifiying code.

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,12 +23,43 @@ module.exports = function(grunt) {
         files: 'scss/**/*.scss',
         tasks: ['sass']
       }
-    }
+    },
+	
+	concat: {   
+	    dist: {
+	        src: [
+	            'bower_components/foundation/js/vendor/modernizr.js',
+				'bower_components/angular/angular.js',
+				'bower_components/angular-route/angular-route.min.js',
+				'bower_components/jquery/jquery.js',
+				'bower_components/lodash/dist/lodash.underscore.min.js',
+				'bower_components/restangular/dist/restangular.min.js',
+				'bower_components/angular-foundation/mm-foundation-tpls.min.js',													
+				'bower_components/foundation/js/foundation.min.js',	
+				'bower_components/svg.js/dist/svg.min.js',	
+				'js/app.js'																		
+				
+	        ],
+	        dest: 'js/build/app.js',
+	    }
+	},
+	
+	uglify: {
+	    build: {
+	        src: 'js/build/app.js',
+	        dest: 'js/build/app.min.js'
+	    }
+	}
+	
   });
+  
+  
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('build', ['sass']);
-  grunt.registerTask('default', ['build','watch']);
+  grunt.registerTask('default', ['build','concat','uglify','watch'])
 }
